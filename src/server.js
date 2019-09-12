9298c37b40c1ab9b5e0f8c6440526f17545096cd
kidow <wcgo2ling@hanmail.net>
2019-09-12 23:29:28 +0900

    pug set

--- src/server.js
+++ src/server.js
@@ -1,1 +1,15 @@
-import express from "express";
+import express from 'express'
+import { join } from 'path'
+import socketIO from 'socket.io'
+
+const PORT = 4000
+const app = express()
+
+app.set('view engine', 'pug')
+app.set('views', join(__dirname, 'views'))
+app.use(express.static(join(__dirname, 'static')))
+app.get('/', (req, res) => res.render('home'))
+
+app.listen(PORT, () => {
+  console.log('App listening on port 4000!')
+})
