0aa7e390b0b6a18f596fd0c871f8356c6c229464
kidow <wcgo2ling@hanmail.net>
2019-09-14 15:23:13 +0900

    socket set

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import { join } from 'path'
 import socketIO from 'socket.io'
+import logger from 'morgan'
 
 const PORT = 4000
 const app = express()
 
 app.set('view engine', 'pug')
 app.set('views', join(__dirname, 'views'))
+app.use(logger('dev'))
 app.use(express.static(join(__dirname, 'static')))
 app.get('/', (req, res) => res.render('home'))
 
@@ -14,4 +16,8 @@ const server = app.listen(PORT, () => {
   console.log('App listening on port 4000!')
 })
 
-const io = socketIO(server)
+const io = socketIO.listen(server)
+
+io.on('connection', socket => {
+  socket.on('helloGuys', _ => console.log('the client says hello'))
+})
